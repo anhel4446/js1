6fcace0313f296537e74fef75e87ddf169f5a638
Khumoyun <anhel4446@gmail.com>
2023-08-18 15:11:44 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var str = prompt('Wht is your name?')
+var str2 = prompt('How old are you?')
+var str3 = prompt('Example 1: 6 + 9 = ?')
+var str4 = prompt('Example 2: 20 - 9 = ?')
+var str5 = prompt('Example 3: 2 * 2 = ?')
+var str6 = prompt('Example 3: 2 / 2 = ?')
+var str7 = prompt('Example 3: 10 % 1 = ?')
+
+let message = "Are you ready to new task?";
+alert (message);
+
+var str8 = +prompt('Input First number :')
+var str9 = +prompt('Input Second number :')
+var str10 = +prompt('Input Third number :')
+
+let message2 = ('The average between the three numbers is ' + (str8 + str9 + str10) / 3 );
+alert(message2);
+
+
+
+console.log('My name is ' + str);
+console.log('I am ' + str2);
+console.log('Example 1: 6 + 9 = 15 Your answer ' + str3);
+console.log('Example 2: 20 - 9 = 11 Your answer ' + str4);
+console.log('Example 3: 2 * 2 = 5 Your answer ' + str5);
+console.log('Example 4: 2 / 2 = 1 Your answer ' + str6);
+console.log('Example 5: 10 % 3 = 1 Your answer ' + str7);
+console.log('The average between numbers ' + str8 + ', ' + str9 + ', ' + str10 + ', ' +  ' is ' +  (str8 + str9 + str10) / 3);
